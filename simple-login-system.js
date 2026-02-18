241c42cdd08071781734dce8bd1b015de9bd1032
tseganekir722-design <tseganekir722@gmail.com>
2026-02-18 11:10:48 +0200

    Add files via upload
    simple login system

---
+++ simple-login-system.js
@@ -0,0 +1,8 @@
+let username = prompt("Enter username:");
+let password = prompt("Enter password:");
+
+if (username === "admin" && password === "1234") {
+    console.log("Login Successful");
+} else {
+    console.log("Invalid Credentials");
+}
